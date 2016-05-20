2fc9a7179ce30782c5fcb812371a96e2445f747a
Damian Przybylski <vazzonn@wp.pl>
2016-05-20 12:46:40 +0200

    Dodano czysty kod openGL do main.cpp
    Dodano czysty kod openGL do main.cpp w folderach OSX i WINDOWS oraz
    stworzono folder CPP, w razie gdyby nie były potrzebne rozgraniczenia na
    systemy OSX i WINDOWS.

---
+++ WINDOWS/main.cpp
@@ -0,0 +1,87 @@
+#if defined __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+//#include <GL/gl.h>
+//#include <GL/glu.h>
+#include <GL/freeglut.h>
+#endif
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+void init()
+{
+	
+}
+
+
+void display()
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPushMatrix();
+	
+	glPopMatrix();
+	
+	glutPostRedisplay();
+	glutSwapBuffers();
+}
+
+
+void loop()
+{
+	
+	Sleep(1);
+	glutPostRedisplay();
+}
+
+void keyDown(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:
+		exit(0);
+		break;
+	}
+}
+
+void keyUp(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+
+	}
+}
+
+void mouseFunc(int x_d, int y_d)
+{
+	glutPostRedisplay();
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(600, 600);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Age of GLUT");
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glOrtho(-20.0, 20.0, -20.0, 20.0, -20.0, 20.0);
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyDown);
+	glutKeyboardUpFunc(keyUp);
+	glutPassiveMotionFunc(mouseFunc);
+	glutIdleFunc(loop);
+
+	init();
+	glutMainLoop();
+
+	return 1;
+}
